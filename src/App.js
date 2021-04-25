271c7667fb6e7499febefedac91c9b0ae3b4db83
SIROKUMA <cooperatehyo09pr@gmail.com>
2021-04-25 23:15:45 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({ favrite }) {
+  return (
+    <h1>I love {favrite}</h1>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!!!!!!!!!!</h1>
+      <Food favrite="kimchi" />
+      <Food favrite="ramen" />
+      <Food favrite="cola" />
+      <Food favrite="butabara" />
     </div>
   );
 }
